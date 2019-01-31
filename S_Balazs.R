81058c3f3d225e7f050d54d3c15b5e9114950c98
SomogyiBalazsA <sobalbundi@gmail.com>
2019-01-31 09:55:34 +0100

    add

---
+++ S_Balazs.R
@@ -0,0 +1,13 @@
+install.packages("gapminder", "tidyverse")
+library(gapminder)
+library(tidyverse)
+library(dplyr)
+
+summary(gapminder)
+str(gapminder)
+
+filter(gapminder, lifeExp == min(lifeExp))
+
+
+x <- filter(gapminder, lifeExp > 60 , gdpPercap < 7000)       
+y <- filter(gapminder, lifeExp > 60 | gdpPercap < 7000)       
