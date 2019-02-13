9634fea8f9df40c0b42173b680b75b1af82e0e91
herczegrobert <herczeg.robert@pte.hu>
2019-02-13 09:28:48 +0100

    none

--- robert.R
+++ robert.R
@@ -44,3 +44,103 @@ filter(gapminder, lifeExp > 60 | gdp < 7000)
 
 
 
+library(gapminder)
+
+summary(gapminder)
+str(gapminder)
+# oszlopok nevét adja meg
+colnames(gapminder)
+
+
+library(dplyr)
+
+dplyr::filter(gapminder,
+              country == "Hungary",
+              year > 2000)
+
+
+cats <- data.frame(coat = c("calico", "black", "tabby"),
+                   weight = c(2.1, 5, 3.2),
+                   like_string = c(1, 0, 1))
+
+
+?data.frame
+
+View(cats)
+
+
+
+
+myVector <- vector(length = 3, mode = 'character')
+myVector
+str(myVector)
+
+v1 <- c(1, 2)
+v2 <- c(3, 4)
+
+?c
+
+v3 <- c(v1, v2)
+v3
+
+v3 <- c(v2, v1)
+v3
+
+
+
+
+
+
+
+
+
+
+
+v1 <- c(1, 2)
+v2 <- c("szerda")
+v3 <- c(v1, v2)
+v3
+
+
+v1 <- c(1, 2)
+v2 <- c( "a", FALSE)
+v3 <- c(v1, v2)
+v3
+
+
+v1 <- c(1, 0)
+v2 <- c(TRUE, FALSE)
+v3 <- c(v1, v2)
+v3
+
+
+
+summary(cats)
+
+
+c(1, 3, 5)
+
+cats
+
+cats[1:3]
+cats[c(1, 3)]
+cats[c(1, 3, 2)]
+cats[c(1, 2, 3)]
+
+cats[4]
+
+
+cats[1:3, ]
+
+cats[1:40, ]
+
+cats[c(1, 3), ]
+cats[-2,]
+cats[c(-1, -3), ]
+
+cats[-2]
+cats[1, 2:3]
+cats[c(1, 3), 2:3]
+
+
+dplyr::filter(cats, bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 +uj)
